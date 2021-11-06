e824b2a6b76ad4f474e3d990fbb85a43a6c14b71
maxlaza411 <53149014+maxlaza411@users.noreply.github.com>
2021-11-06 12:15:11 -0400

    Init

--- setup.py
+++ setup.py
@@ -14,49 +14,57 @@ with open(os.path.join(os.path.dirname(__file__), 'xlwings', '__init__.py')) as 
 
 # Dependencies
 if sys.platform.startswith('win'):
-    install_requires = ['comtypes']
+    if sys.version_info[:2] >= (3, 7):
+        pywin32 = 'pywin32 >= 224'
+    else:
+        pywin32 = 'pywin32'
+    install_requires = [pywin32]
     # This places dlls next to python.exe for standard setup and in the parent folder for virtualenv
     data_files = [('', glob.glob('xlwings*.dll'))]
 elif sys.platform.startswith('darwin'):
     install_requires = ['psutil >= 2.0.0', 'appscript >= 1.0.1']
-    data_files = [(os.path.expanduser("~") + '/Library/Application Scripts/com.microsoft.Excel', ['xlwings/xlwings.applescript'])]
+    data_files = [(os.path.expanduser("~") + '/Library/Application Scripts/com.microsoft.Excel', [f'xlwings/xlwings-{version}.applescript'])]
 else:
-    on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
-    if on_rtd:
+    if os.environ.get('READTHEDOCS', None) == 'True' or os.environ.get('INSTALL_ON_LINUX') == '1':
         data_files = []
         install_requires = []
     else:
-        raise OSError("currently only Windows and OSX are supported.")
+        raise OSError("xlwings requires an installation of Excel and therefore only works on Windows and macOS. To enable the installation on Linux nevertheless, do: export INSTALL_ON_LINUX=1; pip install xlwings")
 
-# This shouldn't be necessary anymore as we dropped official support for < 2.7 and < 3.3
-if (sys.version_info[0] == 2 and sys.version_info[:2] < (2, 7)) or (sys.version_info[0] == 3 and sys.version_info[:2] < (3, 2)):
-    install_requires = install_requires + ['argparse']
+extras_require = {
+    'pro': ['cryptography', 'Jinja2', 'pdfrw'],
+    'all': ['cryptography', 'Jinja2', 'pandas', 'matplotlib', 'plotly', 'flask', 'requests', 'pdfrw']
+}
 
 setup(
     name='xlwings',
     version=version,
-    url='http://xlwings.org',
+    url='https://www.xlwings.org',
     license='BSD 3-clause',
     author='Zoomer Analytics LLC',
     author_email='felix.zumstein@zoomeranalytics.com',
     description='Make Excel fly: Interact with Excel from Python and vice versa.',
     long_description=readme,
     data_files=data_files,
-    packages=find_packages(),
-    package_data={'xlwings': ['tests/*.xlsx', 'tests/*.xlsm', 'tests/*.png',
-                              '*.xlsm', 'xlwings.applescript',
-                              'addin/xlwings.xlam']},
+    packages=find_packages(exclude=('tests', 'tests.*',)),
+    package_data={'xlwings': ['xlwings.bas', 'Dictionary.cls', '*.xlsm', '*.xlam', '*.applescript', 'addin/xlwings.xlam', 'addin/xlwings_unprotected.xlam']},
     keywords=['xls', 'excel', 'spreadsheet', 'workbook', 'vba', 'macro'],
     install_requires=install_requires,
-    entry_points={'console_scripts': ['xlwings=xlwings.command_line:main'],},
+    extras_require=extras_require,
+    entry_points={'console_scripts': ['xlwings=xlwings.cli:main'],},
     classifiers=[
         'Development Status :: 4 - Beta',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: MacOS :: MacOS X',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
         'Topic :: Office/Business :: Financial :: Spreadsheet',
         'License :: OSI Approved :: BSD License'],
     platforms=['Windows', 'Mac OS X'],
+    python_requires='>=3.6',
 )
